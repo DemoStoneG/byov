a84e52032e218d84d01858051eab054c3901d57d
DemoStone <2607848203@qq.com>
2026-07-13 20:54:38 +0800

    starting test01

--- train.py
+++ train.py
@@ -2,10 +2,6 @@ import logging
 import os
 import sys
 
-from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks import Callback, ModelCheckpoint
-from pytorch_lightning.loggers import CSVLogger, TensorBoardLogger
-
 from utils.config import argparser
 from utils.experiment import (
     maybe_create_outputs_symlink,
@@ -14,27 +10,30 @@ from utils.experiment import (
     setup_logging,
     update_best_checkpoint,
 )
-from utils.util import CustomModelCheckpoint
-from video_tasks import VideoAlignment
-
-
-class BestCheckpointMetadata(Callback):
-    def __init__(self, checkpoint_callback):
-        self.checkpoint_callback = checkpoint_callback
-
-    def on_validation_end(self, trainer, pl_module):
-        update_best_checkpoint(
-            self.checkpoint_callback.best_model_path,
-            self.checkpoint_callback.best_model_score,
-            pl_module.args.metrics_dir,
-            pl_module.args.checkpoints_dir,
-        )
-
-    def on_train_end(self, trainer, pl_module):
-        self.on_validation_end(trainer, pl_module)
 
 
 def build_trainer(args):
+    from pytorch_lightning import Trainer
+    from pytorch_lightning.callbacks import Callback, ModelCheckpoint
+    from pytorch_lightning.loggers import CSVLogger, TensorBoardLogger
+
+    from utils.util import CustomModelCheckpoint
+
+    class BestCheckpointMetadata(Callback):
+        def __init__(self, checkpoint_callback):
+            self.checkpoint_callback = checkpoint_callback
+
+        def on_validation_end(self, trainer, pl_module):
+            update_best_checkpoint(
+                self.checkpoint_callback.best_model_path,
+                self.checkpoint_callback.best_model_score,
+                pl_module.args.metrics_dir,
+                pl_module.args.checkpoints_dir,
+            )
+
+        def on_train_end(self, trainer, pl_module):
+            self.on_validation_end(trainer, pl_module)
+
     periodic_checkpoint = CustomModelCheckpoint(
         every_n_epochs=args.save_every,
         dirpath=args.checkpoints_dir,
@@ -74,6 +73,10 @@ def build_trainer(args):
 
 
 def main(args):
+    from pytorch_lightning import seed_everything
+
+    from video_tasks import VideoAlignment
+
     logger = logging.getLogger(__name__)
     seed_everything(args.seed, workers=True)
     task = VideoAlignment(args)
@@ -98,6 +101,9 @@ if __name__ == "__main__":
     args, is_resume = prepare_experiment(cli_args)
     logger = setup_logging(args.logs_dir)
     save_run_metadata(args, sys.argv, is_resume)
-    maybe_create_outputs_symlink(args.output_root)
     logger.info("Prepared run directory: %s", args.run_dir)
+    if args.dry_run == "config":
+        logger.info("Dry run config complete. Training dependencies, backbone, and dataset were not loaded.")
+        sys.exit(0)
+    maybe_create_outputs_symlink(args.output_root)
     main(args)
